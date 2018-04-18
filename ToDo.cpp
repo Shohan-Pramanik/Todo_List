9a923e12c7c5d69d97bd1e144f3c0f7fdebc32ba
Shohan-Pramanik <shohan.pramanik38@gmail.com>
2018-04-19 01:57:00 +0600

    Add information to the file.

--- ToDo.cpp
+++ ToDo.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+fstream io;
 class todo{
     private:
         int tmp;
@@ -17,6 +18,23 @@ class todo{
         void manual();
         void check();
 };
+
+void  todo :: add(){
+        io.open("todo.txt",ios::app);
+        t_name();
+        getchar();
+        getline(cin,task_name);
+        t_description();
+        getline(cin,task_description);
+        t_date();
+        getline(cin,task_date);
+        io<<task_name<<endl;
+        io<<task_description<<endl;
+        io<<task_date<<endl;
+        io.close();
+        manual();
+
+}
 
 int main()
 {
