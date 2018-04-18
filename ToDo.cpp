5df4e8bedc1b42521ff9e21919ab0e472be314bc
Shohan-Pramanik <shohan.pramanik38@gmail.com>
2018-04-19 02:03:14 +0600

    To delete the information.

--- ToDo.cpp
+++ ToDo.cpp
@@ -83,6 +83,31 @@ void  todo :: show(){
         manual();
 }
 
+void todo :: delet(){
+    cout<<"Which one do you want to delete?\n";
+    cin>>tmp;
+    ofstream temp;
+    io.open("todo.txt");
+    temp.open("temp.txt",ios::out);
+    int i=1;
+    while(!io.eof()){
+            getline (io,task_name);
+            getline (io,task_description);
+            getline (io,task_date);
+            if(i!=tmp && !io.eof()){
+                temp<<task_name<<endl;
+                temp<<task_description<<endl;
+                temp<<task_date<<endl;
+            }
+            i++;
+    }
+    temp.close();
+    io.close();
+    remove("todo.txt");
+    rename("temp.txt","todo.txt");
+    manual();
+}
+
 int main()
 {
 
